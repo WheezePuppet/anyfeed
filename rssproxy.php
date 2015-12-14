be5a6c41f307359ead6e20a6ba568d29e4b54010
Stephen <stephen@umw.edu>
2015-12-14 15:29:03 -0500

    Set user agent.

--- rssproxy.php
+++ rssproxy.php
@@ -1,6 +1,10 @@
 <?php
     header("Content-Type: text/xml");
     $url = $_GET['url'];
+
+    /* Ian Finlayson is a genius */
+    ini_set('user_agent','Mozilla/5.0');
+
     print file_get_contents($url);
     flush();
 ?>
